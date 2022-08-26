33b5ff259802598f62ef26fff0681fe50b0ff081
Rajas Kale <rajas.kale@anthem.com>
2022-08-25 21:11:17 -1000

    1

--- src/table15/runner.py
+++ src/table15/runner.py
@@ -34,117 +34,115 @@ def run(configs_path='../configs/pima_diabetes.yaml'):
         baselines = [None]
 
     print('getting magecs...')
-    # with mp.Manager() as manager:
-    #     run_dfs = manager.dict()
-    #     processes = []
-    #     keys = []
-    #     for baseline in baselines:
-    #         print(models_dict)
-    #         for model in models_dict.keys():
-    #             key = model + '_p{}'.format(int(baseline * 100)) if baseline not in [None, 'None'] else model + '_0'
-    #             keys.append(key)
-    #             clf = models_dict[model]
-    #             if model in ['mlp', 'lstm']:
-    #                 clf = clf.model
-    #             print('in1')
-    #             p = mp.Process(name=key, target=run_magecs, 
-    #                 args=(run_dfs, clf, x_validation_p, y_validation_p, model, baseline, features))
-    #             print('in11')
-    #             processes.append(p)
+    with mp.Manager() as manager:
+        run_dfs = manager.dict()
+        processes = []
+        keys = []
+        for baseline in baselines:
+            print(models_dict)
+            for model in models_dict.keys():
+                key = model + '_p{}'.format(int(baseline * 100)) if baseline not in [None, 'None'] else model + '_0'
+                keys.append(key)
+                clf = models_dict[model]
+                if model in ['mlp', 'lstm']:
+                    clf = clf.model
+                p = mp.Process(name=key, target=run_magecs, 
+                    args=(run_dfs, clf, x_validation_p, y_validation_p, model, baseline, features))
+                print('in11')
+                processes.append(p)
     
-    #     for p in processes:
-    #         p.start()
-    #         print(p)
-    #     for p in processes:
-    #         p.join()
+        for p in processes:
+            p.start()
+        for p in processes:
+            p.join()
 
-    # TODO: fix multiprocessing
-    run_dfs = dict()
-    keys = []
-    for baseline in baselines:
-        for model in models_dict.keys():
-            key = model + '_p{}'.format(int(baseline * 100)) if baseline not in [None, 'None'] else model + '_0'
-            keys.append(key)
-            clf = models_dict[model]
-            if model in ['mlp', 'lstm']:
-                clf = clf.model
-            run_dfs[key] = run_magecs(clf, x_validation_p, y_validation_p, model, key, baseline, features)
+    # # TODO: fix multiprocessing
+    # run_dfs = dict()
+    # keys = []
+    # for baseline in baselines:
+    #     for model in models_dict.keys():
+    #         key = model + '_p{}'.format(int(baseline * 100)) if baseline not in [None, 'None'] else model + '_0'
+    #         keys.append(key)
+    #         clf = models_dict[model]
+    #         if model in ['mlp', 'lstm']:
+    #             clf = clf.model
+    #         run_dfs[key] = run_magecs(clf, x_validation_p, y_validation_p, model, key, baseline, features)
 
-    # TODO: Def this process:
-    baseline_runs = defaultdict(list)
-    # keys = sorted(keys)
-    for key in keys:
-        baseline = key.split('_')[1]
-        if baseline[0] == 'p':
-            baseline = int(baseline[1:]) / 100
-        else:
-            baseline = int(baseline)
-        yaml_check = baseline
-        if baseline == 0:
-            yaml_check = None
-        assert yaml_check in baselines
-        baseline_runs[baseline].append(run_dfs[key])
+        # TODO: Def this process:
+        baseline_runs = defaultdict(list)
+        # keys = sorted(keys)
+        for key in keys:
+            baseline = key.split('_')[1]
+            if baseline[0] == 'p':
+                baseline = int(baseline[1:]) / 100
+            else:
+                baseline = int(baseline)
+            yaml_check = baseline
+            if baseline == 0:
+                yaml_check = None
+            assert yaml_check in baselines
+            baseline_runs[baseline].append(run_dfs[key])
     
-    # print(run_dfs.keys())
-    # print(baseline_runs.keys())
+        # print(run_dfs.keys())
+        # print(baseline_runs.keys())
 
-    # print(len(run_dfs['lr_p50']))
-    # print(len(baselines[0,5]))
+        # print(len(run_dfs['lr_p50']))
+        # print(len(baselines[0,5]))
 
-    # TODO: Def this process:
-    baseline_to_scores_df = {}
-    all_joined = {}
-    for baseline, model_runs in baseline_runs.items():
-        baseline_joined = mg.magec_models(*model_runs,
-                            Xdata=x_validation_p,
-                            Ydata=y_validation_p,
-                            features=features)
-        baseline_ranked_df = mg.magec_rank(baseline_joined, rank=len(features), features=features, models=models)
-        scores_df = agg_scores(baseline_ranked_df, policy=policy, models=models)
+        # TODO: Def this process:
+        baseline_to_scores_df = {}
+        all_joined = {}
+        for baseline, model_runs in baseline_runs.items():
+            baseline_joined = mg.magec_models(*model_runs,
+                                Xdata=x_validation_p,
+                                Ydata=y_validation_p,
+                                features=features)
+            baseline_ranked_df = mg.magec_rank(baseline_joined, rank=len(features), features=features, models=models)
+            scores_df = agg_scores(baseline_ranked_df, policy=policy, models=models)
 
-        all_joined[baseline] = baseline_joined
-        baseline_to_scores_df[baseline] = scores_df
+            all_joined[baseline] = baseline_joined
+            baseline_to_scores_df[baseline] = scores_df
 
-    output_logits = {}
-    output_probs = {}
+        output_logits = {}
+        output_probs = {}
 
-    for baseline in baselines:
-        if baseline is None:
-            baseline = 0
-        df_logits = pd.DataFrame.from_records(baseline_to_scores_df[baseline]['logits'])
-        df_probs = pd.DataFrame.from_records(baseline_to_scores_df[baseline]['probs'])
+        for baseline in baselines:
+            if baseline is None:
+                baseline = 0
+            df_logits = pd.DataFrame.from_records(baseline_to_scores_df[baseline]['logits'])
+            df_probs = pd.DataFrame.from_records(baseline_to_scores_df[baseline]['probs'])
 
-        if baseline in [None, 0]:
-            baseline = 1.0
-        base_logits_strings = get_string_repr(df_logits, features)
-        base_probs_strings = get_string_repr(df_probs, features)
+            if baseline in [None, 0]:
+                baseline = 1.0
+            base_logits_strings = get_string_repr(df_logits, features)
+            base_probs_strings = get_string_repr(df_probs, features)
 
-        output_logits[baseline] = base_logits_strings
-        output_probs[baseline] = base_probs_strings
-    
-    # TODO: fix baselines upstream  to handle None as 0
-    if None in baselines:
-        idx = baselines.index(None)
-        baselines[idx] = 1.0
-    
-    df_logits_out = pd.DataFrame.from_records(output_logits)
-    df_logits_out['feature'] = features
-    # re-order cols
-    cols = ['feature'] + baselines
-    df_logits_out = df_logits_out.rename(columns={'0': 'full'})
-    df_logits_out = df_logits_out[cols]
+            output_logits[baseline] = base_logits_strings
+            output_probs[baseline] = base_probs_strings
+        
+        # TODO: fix baselines upstream  to handle None as 0
+        if None in baselines:
+            idx = baselines.index(None)
+            baselines[idx] = 1.0
+        
+        df_logits_out = pd.DataFrame.from_records(output_logits)
+        df_logits_out['feature'] = features
+        # re-order cols
+        cols = ['feature'] + baselines
+        df_logits_out = df_logits_out.rename(columns={'0': 'full'})
+        df_logits_out = df_logits_out[cols]
 
-    df_probs_out = pd.DataFrame.from_records(output_probs)
-    df_probs_out['feature'] = features
-    # re-order cols
-    cols = ['feature'] + baselines
-    df_probs_out = df_probs_out.rename(columns={'0': 'full'})
-    df_probs_out = df_probs_out[cols]
+        df_probs_out = pd.DataFrame.from_records(output_probs)
+        df_probs_out['feature'] = features
+        # re-order cols
+        cols = ['feature'] + baselines
+        df_probs_out = df_probs_out.rename(columns={'0': 'full'})
+        df_probs_out = df_probs_out[cols]
 
-    print(df_logits_out.head())
-    print(df_probs_out.head())
+        print(df_logits_out.head())
+        print(df_probs_out.head())
 
-    return (df_logits_out, df_probs_out), all_joined
+        return (df_logits_out, df_probs_out), all_joined
 
 
 def agg_scores(ranked_df, policy='mean', models=('mlp', 'rf', 'lr')):
